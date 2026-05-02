3f5c38e4ea72df9e67a4974f01ccdfa41f5d89e8
novitas0312-cmd <novita.s0312@gmail.com>
2026-05-02 19:22:23 +0200

    ubah code bagian tandatangan
    code nomer 2333 sekarang di nomor 235

--- BK.py
+++ BK.py
@@ -230,12 +230,10 @@ def create_pdf(data, image_file=None, sat_url=None, logo_file=None): # Tambah pa
     pdf.set_font("Helvetica", "I", 10)
     pdf.cell(70, 8, "Unterschrift Prüfer", ln=True, align="C")
 
-        if pdf.get_y() > 250: # Falls wir schon fast am Seitenende sind
-        pdf.add_page()    # Neue Seite anfangen
 
-
-    
     pdf_output = pdf.output()
+    if pdf.get_y() > 250: # Falls wir schon fast am Seitenende sind
+        pdf.add_page()    # Neue Seite anfangen
     if isinstance(pdf_output, bytearray):
          return bytes(pdf.output())
 
