f1fc9165be1856ba2364b76f80c8b9741687d689
novitas0312-cmd <novita.s0312@gmail.com>
2026-05-04 14:35:55 +0200

    create_pdf

--- BK.py
+++ BK.py
@@ -222,12 +222,7 @@ def create_pdf(data, image_file=None, sat_url=None, logo_file=None):
             except Exception as e:
                 pdf.set_font("Helvetica", "I", 10)
                 pdf.text(x=105, y=bild_y + 10, text=f"Sat-Bild Fehler: {e}")
-                
     pdf_result = pdf.output()
-
-    # 2. Sicherstellen, dass es im Format 'bytes' zurückgegeben wird
-    if isinstance(pdf_result, (bytearray, bytes)):
-        return bytes(pdf_result)
     
     # --- UNTERSCHRIFTENFELD (Ganz am Ende) ---
     pdf.ln(20) # Großer Abstand nach oben
